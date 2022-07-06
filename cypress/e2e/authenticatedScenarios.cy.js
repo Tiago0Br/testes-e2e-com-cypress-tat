cd6c2cf2e259bc259b02f5f10d8fc0201a08c929
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-07-06 20:57:56 -0300

    Criação de testes de ações que exigem autenticação
    - CRUD (cadastro, leitura, edição e exclusão) de notas
    - Preenchimento do formulário de cartão de crédito

---
+++ cypress/e2e/authenticatedScenarios.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+const faker = require('faker')
+
+describe('CRUD notes and fill Settings form', () => {
+  beforeEach(() => {
+    cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'), { cacheSession: false })
+  })
+
+  it('Cread, read, update and delete a note', () => {
+    const note = faker.lorem.words(4)
+    cy.createNote(note)
+
+    const updatedNote = faker.lorem.words(4)
+    cy.updateNote(note, updatedNote, 'example.json')
+
+    cy.deleteNote(updatedNote)
+  })
+
+  it('Fill settings form and submit', () => {
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
+    cy.fillSettingsFormAndSubmit(5, 'Joãozinho', Cypress.env('CREDIT_CARD'))
+    cy.wait('@paymentRequest').then(({ state, response }) => {
+      expect(state).to.be.equal('Complete')
+      expect(response.statusCode).to.be.equal(200)
+    })
+  })
+})
