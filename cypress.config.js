558c25e9536a0ea559be7df8b9f122b775cce578
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-07-07 19:19:04 -0300

    Integrando os testes com o Cypress Dashboard
    - Adicionando o projectId
    - Adicionando a variável de ambiente RECORD_KEY
    - Alterando a configuração da CI
    - Alterando os scripts para integrar com o Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'https://notes-serverless-app.com',
     experimentalSessionAndOrigin: true,
     chromeWebSecurity: false,
+    projectId: 'ejnqi2',
     env: {
       viewportWidthBreakpoint: 768
     }
